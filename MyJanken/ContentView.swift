858cb34a609c3e0d6e8f66b496c68e20cba0c637
netmullet <lipper.corp@gmail.com>
2024-05-26 16:58:56 +0900

    Initial Commit

---
+++ MyJanken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyJanken
+//
+//  Created by Otsuka on 2024/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by Otsuka on 2024/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
